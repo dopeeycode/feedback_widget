bcf6d7a8002ecab3028c6e507bcc535b5418b140
Italo <italodopeey@gmail.com>
2023-06-14 21:15:01 -0300

    feat: criação funcionalidade principal do feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,19 +3,34 @@ import { CloseButton } from '../../CloseButton'
 import { feedbackTypes } from '../../../utils/feedbackTypes'
 import { ArrowLeft } from 'phosphor-react'
 import ScreenshotButton from '../ScreenshotButton'
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType
   onFeedbackRestartRequested: () => void
+  onFeedbackSent: () => void
 }
 
 export default function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
+  onFeedbackSent,
 }: FeedbackContentStepProps) {
-  const feedbackTypeInfo = feedbackTypes[feedbackType]
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault()
+
+    console.log({
+      comment,
+      screenshot,
+    })
+
+    onFeedbackSent()
+  }
 
   return (
     <>
@@ -39,30 +54,33 @@ export default function FeedbackContentStep({
 
         <CloseButton />
       </header>
-      <form className="mt-4 my-2 w-full">
+      <form onSubmit={handleSubmitFeedback} className="mt-4 my-2 w-full">
         <textarea
           className="min-w-[304px] max-sm:min-w-[250px] w-full min-h-[112px] text-sm placeholder-zinc-400 
           text-zinc-100 border-[#52525B] bg-transparent rounded-md border py-2 px-3 focus:border-brand-500
         focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700
           scrollbar-track-transparent scrollbar-thin"
+          onChange={(e) => setComment(e.target.value)}
           placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         />
-      </form>
 
-      <footer className="flex gap-2 w-full">
-        <ScreenshotButton
-          onScreenshotTook={setScreenshot}
-          screenshot={screenshot}
-        />
-        <button
-          type="submit"
-          className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center 
-          text-sm hover:bg-[#996DFF] transition-colors focus:ring-2 focus:ring-offset-2 
-          focus:ring-offset-zinc-900 focus:ring-brand-500"
-        >
-          Enviar feedback
-        </button>
-      </footer>
+        <footer className="flex gap-2 w-full">
+          <ScreenshotButton
+            onScreenshotTook={setScreenshot}
+            screenshot={screenshot}
+          />
+          <button
+            type="submit"
+            disabled={!comment}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center 
+            items-centertext-sm hover:bg-[#996DFF] transition-colors focus:ring-2 focus:ring-offset-2 
+          focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-60 disabled:hover:bg-brand-500
+            "
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   )
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 import { feedbackTypes } from '../../utils/feedbackTypes'
 import FeedbackTypeStep from './Steps/FeedbackTypeStep'
 import FeedbackContentStep from './Steps/FeedbackContentStep'
+import FeedbackSuccessStep from './Steps/FeedbackSuccessStep'
 
 export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null)
   }
 
@@ -18,13 +21,22 @@ export function WidgetForm() {
       className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg
      md:w-auto w-[calc(100vw-2rem)]"
     >
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-      ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
+      {feedbackSent ? (
+        <FeedbackSuccessStep
           onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs mt-2 text-neutral-400">
